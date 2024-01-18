ed3196711f71cf90052d17469ea88c7902311262
Fenil <fenil.rami@bacancy.com>
2024-01-18 17:07:58 +0530

    Adds: task_6_constants

---
+++ week1/task_6_constants.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+const c = 500;
+
+// c = 1000; // It will give a Typerror (Assignment to constant variable)
+
+// So in javascript the const keyword is used to declare constant variables and value of these variables can't be altered/changed
+// const variables must be initialized
+
+// const a; // It will give a syntax error (Missing initializer in const declaration)
+
+// use of const
+
+// constant variabled can be used to declare variables whose values shouldn't be changed
+
+// example
+const SERVER_API_KEY = 'THIS_IS_SECRET_API_KEY_FOR_SERVER';
+const PI = 3.14159265359;
