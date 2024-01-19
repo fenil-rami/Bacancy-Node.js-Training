271a9b90bf5e2200d9f52804d01ceae477adaea7
Fenil <fenil.rami@bacancy.com>
2024-01-19 11:38:42 +0530

    Adds: task_9_numbers_and_strings

---
+++ week1/task_9_numbers_and_strings.js
@@ -0,0 +1,10 @@
+/* eslint-disable prefer-template */
+/* eslint-disable no-console */
+/* eslint-disable quotes */
+/* eslint-disable no-unused-vars */
+const age = 21;
+const name = 'Fenil';
+const isStudent = true;
+
+console.log(`Hii! My name is ${name}. I'm ` + age.toString() + ` years old.` + (isStudent ? ` Currently I'm studying!` : ` Currently I'm working!`));
+// Output : Hii! My name is Fenil. I'm 21 years old. Currently I'm studying!
