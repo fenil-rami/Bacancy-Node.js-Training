7aeb6b5f7718b26227924a695c891b371ce6a434
Fenil <fenil.rami@bacancy.com>
2024-01-31 12:04:47 +0530

    Adds: http status codes

---
+++ week4/task_4_http_codes.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+// Commonly used http status codes
+const responseCode = {
+  200: 200, // OK
+  201: 201, // Created
+  204: 204, // No Content
+  304: 304, // Content Not Modified
+  400: 400, // Bad Request
+  401: 401, // Unauthorized
+  403: 403, // Forbidden
+  404: 404, // Not Found
+  409: 409, // Conflict
+  422: 422, // Validation Errors
+  500: 500, // Internal Server Error
+};
+
+// All the respective http status code already added in all crud apis
