413248558cab673a8bd76bb3c826a37bfcd54e23
Fenil <fenil.rami@bacancy.com>
2024-02-02 12:55:56 +0530

    Adds: file upload and get using multer

--- week3/task_4_main.js
+++ week3/task_4_main.js
@@ -3,6 +3,7 @@
 
 // // (not able to use both import and require together)
 // import { multiply } from './task_4_multiply.js';
+
 // console.log(multiply(5, 10));
 
 const sum = require('./task_4_sum.cjs');
---
+++ week4/uploads/f176fdf8-f4c3-417c-b5ed-ae757759cbbe.cpp
@@ -0,0 +1,9 @@
+// This is test cpp file which is gonna be uploaded on expressjs server using multerjs
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+cout << "Hello World!" << endl;
+return 0;
+} 
