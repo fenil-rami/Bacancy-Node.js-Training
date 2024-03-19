28fd5508300d0b98b76fee53ebc6cd0e5309f304
Fenil <fenil.rami@bacancy.com>
2024-03-19 16:36:25 +0530

    Adds: typescript task 11

---
+++ week5/task_11.ts
@@ -0,0 +1,7 @@
+declare module 'str-utils' {
+  export const strReverse:(value: string) => string
+  export const strToLower:(value: string) => string
+  export const strToUpper:(value: string) => string
+  export const strRandomize:(value: string) => string
+  export const strInvertCase:(value: string) => string
+}
